aeba1a489acf7afd9130fe318c36818ac4f70059
Edwin Campos Dragusin <132321038+EdwinCaD@users.noreply.github.com>
2023-08-30 22:25:41 -0600

    Update CaballoAjedrez.java

--- CaballoAjedrez/src/caballoajedrez/CaballoAjedrez.java
+++ CaballoAjedrez/src/caballoajedrez/CaballoAjedrez.java
@@ -90,10 +90,69 @@ public class CaballoAjedrez {
                  imprimir("Se ha encontrado la meta");
                  encontrado = true;
              }else{
+                 ArrayList<nodo> listaAuxiliar = new ArrayList();
                  listaNodos.remove(0);
+                 ///Creacion y verificacion de los hijos
                  hijo = auxiliar.arriba1();
+                 listaAuxiliar.add(hijo);
+                 hijo = auxiliar.arriba2();
+                 listaAuxiliar.add(hijo);
+                 hijo = auxiliar.derecha1();
+                 listaAuxiliar.add(hijo);
+                 hijo = auxiliar.derecha2();
+                 listaAuxiliar.add(hijo);
+                 hijo = auxiliar.abajo1();
+                 listaAuxiliar.add(hijo);
+                 hijo = auxiliar.abajo2();
+                 listaAuxiliar.add(hijo);
+                 hijo = auxiliar.izquierda1();
+                 listaAuxiliar.add(hijo);
+                 hijo = auxiliar.izquierda2();
+                 listaAuxiliar.add(hijo);
                  
-                 
+                 for(nodo aux: listaAuxiliar){
+                     if(!existeHistorial(aux,historialNodos) && aux.esValido()){
+                         listaNodos.add(aux);
+                         historialNodos.add(aux);
+                     }
+                 } 
+             }
+             nivel++;
+         }
+         if(encontrado){
+             String mensajeEncontrado="Camino tomado: \n";
+             nodo meta = listaNodos.get(0);
+             for(int movimiento: meta.movimientos){
+                 switch(movimiento){
+                     case 1:
+                         mensajeEncontrado+="Mover arriba a la izquierda \n";
+                         break;
+                     case 2:
+                         mensajeEncontrado+="Mover arriba a la derecha \n";
+                         break;
+                     case 3:
+                         mensajeEncontrado+="Mover a la derecha arriba \n";
+                         break;
+                     case 4:
+                         mensajeEncontrado+="Mover a la derecha abajo \n";
+                         break;
+                     case 5:
+                         mensajeEncontrado+="Mover abajo a la derecha \n";
+                         break;
+                     case 6: 
+                         mensajeEncontrado+="Mover abajo a la izquierda \n";
+                         break;
+                     case 7:
+                         mensajeEncontrado+="Mover a la izquierda abajo \n";
+                         break;
+                     case 8:
+                         mensajeEncontrado+="Mover a la izquierda arriba \n";
+                         break;
+                    default:
+                         mensajeEncontrado +="Hubo un error al recuperar el texto...";
+                            break;
+                 }
+                 imprimir(mensajeEncontrado);
              }
              
          }
