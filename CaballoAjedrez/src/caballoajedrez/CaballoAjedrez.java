ae32c7a56e890df755afa52d2894247d656c67fc
Edwin Campos Dragusin <132321038+EdwinCaD@users.noreply.github.com>
2023-09-03 00:54:30 +0000

    Se añadieron unas lineas de bandera Se añadieron lineas bandera para poder identificar el error lógico a la hora de llegar al nodo meta.

--- CaballoAjedrez/src/caballoajedrez/CaballoAjedrez.java
+++ CaballoAjedrez/src/caballoajedrez/CaballoAjedrez.java
@@ -2,153 +2,177 @@ package caballoajedrez;
 
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
+
 public class CaballoAjedrez {
-    
+
     public static void main(String[] args) {
-        int x,y;
+        int x, y;
         x = solicitarX("Ingrese el valor de x (LETRAS MAYUSCULAS) del nodo incial");
         y = solicitarY("Ingrese el valor de y (NUMERO ENTERO) del nodo inicial");
-        nodo inicio = new nodo(x,y);
-        
+        nodo inicio = new nodo(x, y);
+
         x = solicitarX("Ingrese el valor de x (LETRAS MAYUSCULAS) del nodo final");
         y = solicitarY("Ingrese el valor de y (NUMERO ENTERO) del nodo inicial");
-        nodo fin = new nodo(x,y);
-        busquedaAmplitud(inicio,fin);
+        nodo fin = new nodo(x, y);
+        busquedaAmplitud(inicio, fin);
     }
-    
-    
-    
-    
-    ///METODOS UTILES AL IMPLEMENTAR EL MAIN (Impresion de texto, solicitud de datos
-    public static void imprimir(String texto){
-        JOptionPane.showMessageDialog(null, texto, "Bandera",1);
+
+    /// METODOS UTILES AL IMPLEMENTAR EL MAIN (Impresion de texto, solicitud de
+    /// datos
+    public static void imprimir(String texto) {
+        JOptionPane.showMessageDialog(null, texto, "Bandera", 1);
     }
-    public static void advertencia(String texto){
-        JOptionPane.showMessageDialog(null, texto, "Advertencia",2);
+
+    public static void advertencia(String texto) {
+        JOptionPane.showMessageDialog(null, texto, "Advertencia", 2);
     }
-    public static void error(String texto){
-        JOptionPane.showMessageDialog(null, texto, "Error",0);
+
+    public static void error(String texto) {
+        JOptionPane.showMessageDialog(null, texto, "Error", 0);
     }
-    public static int solicitarX(String mensaje){
-         String texto = JOptionPane.showInputDialog(mensaje);
-         int valor=0;
-         switch(texto){
-             case "A" -> valor = 1;
-             case "B" -> valor = 2;
-             case "C" -> valor = 3;
-             case "D" -> valor = 4;
-             case "E" -> valor = 5;
-             case "F" -> valor = 6;
-             case "G" -> valor = 7;
-             case "H" -> valor = 8;
-             default -> valor=-1;
-         }
-         return valor;
+
+    public static int solicitarX(String mensaje) {
+        String texto = JOptionPane.showInputDialog(mensaje);
+        int valor = 0;
+        switch (texto) {
+            case "A":
+                valor = 1;
+                break;
+            case "B":
+                valor = 2;
+                break;
+            case "C":
+                valor = 3;
+                break;
+            case "D":
+                valor = 4;
+                break;
+            case "E":
+                valor = 5;
+                break;
+            case "F":
+                valor = 6;
+                break;
+            case "G":
+                valor = 7;
+                break;
+            case "H":
+                valor = 8;
+                break;
+            default:
+                valor = -1;
+                break;
+        }
+        return valor;
     }
-     public static int solicitarY(String mensaje){
-         String texto = JOptionPane.showInputDialog(mensaje);
-         int valor=Integer.parseInt(texto);
-         return valor;
+
+    public static int solicitarY(String mensaje) {
+        String texto = JOptionPane.showInputDialog(mensaje);
+        int valor = Integer.parseInt(texto);
+        return valor;
     }
-     ///Funcion para verificar si existe historial del nodo creado
-    public static boolean existeHistorial(nodo x, ArrayList<nodo> historial){
-        for(nodo y:historial){ //para cada nodo en la lista
-            if(y.getX() == x.getX() && y.getY() == x.getY()){ //Si es igual al nodo que buscamos 
-                return true;//Se regresa verdadero
+
+    /// Funcion para verificar si existe historial del nodo creado
+    public static boolean existeHistorial(nodo x, ArrayList<nodo> historial) {
+        for (nodo y : historial) { // para cada nodo en la lista
+            if (y.getX() == x.getX() && y.getY() == x.getY()) { // Si es igual al nodo que buscamos
+                return true;// Se regresa verdadero
             }
         }
-        return false;///De lo contrario, se regresa falso.
+        return false;/// De lo contrario, se regresa falso.
     }
-     public static void busquedaAmplitud(nodo nodoInicial, nodo nodoMeta){
-         ArrayList<nodo> listaNodos = new ArrayList();
-         ArrayList<nodo> historialNodos = new ArrayList();
-         int nivel = 1;
-         boolean encontrado = false;
-         
-         listaNodos.add(nodoInicial);
-         historialNodos.add(nodoInicial);
-         
-         while(!listaNodos.isEmpty() && !encontrado){
-             ///Muestra la lista en el momento de la iteracion
-             String mensaje = "L(" + nivel + "):  ";
-             nodo auxiliar,hijo;
-             for(nodo u: listaNodos){
-                 mensaje += u.imprimir() + ", ";
-             }
-             imprimir(mensaje);
-             
-             ///INICIO DEL ALGORITMO
-             auxiliar = listaNodos.get(0);
-             if(auxiliar.getX() == nodoMeta.getX()){
-                 imprimir("Se ha encontrado la meta");
-                 encontrado = true;
-             }else{
-                 ArrayList<nodo> listaAuxiliar = new ArrayList();
-                 listaNodos.remove(0);
-                 ///Creacion y verificacion de los hijos
-                 hijo = auxiliar.arriba1();
-                 listaAuxiliar.add(hijo);
-                 hijo = auxiliar.arriba2();
-                 listaAuxiliar.add(hijo);
-                 hijo = auxiliar.derecha1();
-                 listaAuxiliar.add(hijo);
-                 hijo = auxiliar.derecha2();
-                 listaAuxiliar.add(hijo);
-                 hijo = auxiliar.abajo1();
-                 listaAuxiliar.add(hijo);
-                 hijo = auxiliar.abajo2();
-                 listaAuxiliar.add(hijo);
-                 hijo = auxiliar.izquierda1();
-                 listaAuxiliar.add(hijo);
-                 hijo = auxiliar.izquierda2();
-                 listaAuxiliar.add(hijo);
-                 
-                 for(nodo aux: listaAuxiliar){
-                     if(!existeHistorial(aux,historialNodos) && aux.esValido()){
-                         listaNodos.add(aux);
-                         historialNodos.add(aux);
-                     }
-                 } 
-             }
-             nivel++;
-         }
-         if(encontrado){
-             String mensajeEncontrado="Camino tomado: \n";
-             nodo meta = listaNodos.get(0);
-             for(int movimiento: meta.movimientos){
-                 switch(movimiento){
-                     case 1:
-                         mensajeEncontrado+="Mover arriba a la izquierda \n";
-                         break;
-                     case 2:
-                         mensajeEncontrado+="Mover arriba a la derecha \n";
-                         break;
-                     case 3:
-                         mensajeEncontrado+="Mover a la derecha arriba \n";
-                         break;
-                     case 4:
-                         mensajeEncontrado+="Mover a la derecha abajo \n";
-                         break;
-                     case 5:
-                         mensajeEncontrado+="Mover abajo a la derecha \n";
-                         break;
-                     case 6: 
-                         mensajeEncontrado+="Mover abajo a la izquierda \n";
-                         break;
-                     case 7:
-                         mensajeEncontrado+="Mover a la izquierda abajo \n";
-                         break;
-                     case 8:
-                         mensajeEncontrado+="Mover a la izquierda arriba \n";
-                         break;
+
+    public static void busquedaAmplitud(nodo nodoInicial, nodo nodoMeta) {
+        ArrayList<nodo> listaNodos = new ArrayList();
+        ArrayList<nodo> historialNodos = new ArrayList();
+        int nivel = 1;
+        boolean encontrado = false;
+
+        listaNodos.add(nodoInicial);
+        historialNodos.add(nodoInicial);
+
+        while (!listaNodos.isEmpty() && !encontrado) {
+            /// Muestra la lista en el momento de la iteracion
+            // String mensaje = "L(" + nivel + "): ";
+            nodo auxiliar, hijo;
+            // for(nodo u: listaNodos){
+            // mensaje += u.imprimir() + ", ";
+            // }
+            // imprimir(mensaje);
+
+            /// INICIO DEL ALGORITMO
+            auxiliar = listaNodos.get(0);
+            if (auxiliar.getX() == nodoMeta.getX() && auxiliar.getY() == nodoMeta.getY()) {
+                imprimir("Se ha encontrado la meta");
+                encontrado = true;
+            } else {
+                ArrayList<nodo> listaAuxiliar = new ArrayList();
+                listaNodos.remove(0);
+                /// Creacion y verificacion de los hijos
+                hijo = auxiliar.arriba1();
+                listaAuxiliar.add(hijo);
+                hijo = auxiliar.arriba2();
+                listaAuxiliar.add(hijo);
+                hijo = auxiliar.derecha1();
+                listaAuxiliar.add(hijo);
+                hijo = auxiliar.derecha2();
+                listaAuxiliar.add(hijo);
+                hijo = auxiliar.abajo1();
+                listaAuxiliar.add(hijo);
+                hijo = auxiliar.abajo2();
+                listaAuxiliar.add(hijo);
+                hijo = auxiliar.izquierda1();
+                listaAuxiliar.add(hijo);
+                hijo = auxiliar.izquierda2();
+                listaAuxiliar.add(hijo);
+
+                for (nodo aux : listaAuxiliar) {
+                    if (!existeHistorial(aux, historialNodos) && aux.esValido()) {
+                        listaNodos.add(aux);
+                        historialNodos.add(aux);
+                        System.out.println("Se ha inroducido el hijo " + aux.imprimir() + " A la lista");
+                    }
+                }
+            }
+            nivel++;
+        }
+        if (encontrado) {
+            String mensajeEncontrado = "Camino tomado: \n";
+            nodo meta = listaNodos.get(0);
+            for (int movimiento : meta.movimientos) {
+                switch (movimiento) {
+                    case 1:
+                        mensajeEncontrado += "Mover arriba a la izquierda \n";
+                        break;
+                    case 2:
+                        mensajeEncontrado += "Mover arriba a la derecha \n";
+                        break;
+                    case 3:
+                        mensajeEncontrado += "Mover a la derecha arriba \n";
+                        break;
+                    case 4:
+                        mensajeEncontrado += "Mover a la derecha abajo \n";
+                        break;
+                    case 5:
+                        mensajeEncontrado += "Mover abajo a la derecha \n";
+                        break;
+                    case 6:
+                        mensajeEncontrado += "Mover abajo a la izquierda \n";
+                        break;
+                    case 7:
+                        mensajeEncontrado += "Mover a la izquierda abajo \n";
+                        break;
+                    case 8:
+                        mensajeEncontrado += "Mover a la izquierda arriba \n";
+                        break;
                     default:
-                         mensajeEncontrado +="Hubo un error al recuperar el texto...";
-                            break;
-                 }
-                
-             }
-              imprimir(mensajeEncontrado);
-         }
-     }
-    
+                        mensajeEncontrado += "Hubo un error al recuperar el texto...";
+                        break;
+                }
+
+            }
+            imprimir(mensajeEncontrado);
+        }
+    }
+
 }
--- CaballoAjedrez/src/caballoajedrez/nodo.java
+++ CaballoAjedrez/src/caballoajedrez/nodo.java
@@ -1,100 +1,115 @@
 package caballoajedrez;
-    
+
 import java.util.ArrayList;
 
 public class nodo {
-    private int x,y;
+    private int x, y;
     public ArrayList<Integer> movimientos = new ArrayList();
 
-    ///DECLARACION DE LOS CONSTRUCTORES DE LA CLASE
-    public nodo (){
-        x=0;
-        y=0;
+    /// DECLARACION DE LOS CONSTRUCTORES DE LA CLASE
+    public nodo() {
+        x = 0;
+        y = 0;
     }
-    public nodo (int var1, int var2) {
-        x=var1;
-        y=var2;
+
+    public nodo(int var1, int var2) {
+        x = var1;
+        y = var2;
     }
-    ///METODOS DE LA CLASE
-    public String imprimir(){
-        char aux='x'; 
-        switch (x){
-            case 1 -> aux = 'A';
-            case 2 -> aux ='B';
-            case 3 -> aux = 'C';
-            case 4 -> aux = 'D';
-            case 5 -> aux = 'E';
-            case 6 -> aux = 'F';
-            case 7 -> aux = 'G';
-            case 8 -> aux = 'H';
+
+    /// METODOS DE LA CLASE
+    public String imprimir() {
+        char aux = 'x';
+        switch (x) {
+            case 1: aux = 'A';
+            case 2: aux = 'B';
+            case 3: aux = 'C';
+            case 4: aux = 'D';
+            case 5: aux = 'E';
+            case 6: aux = 'F';
+            case 7: aux = 'G';
+            case 8: aux = 'H';
         }
-        return "(" + aux +","+y+")";
+        return "(" + aux + "," + y + ")";
     }
-    ///Metodos get y set de las clases.
-    public int getX(){
+
+    /// Metodos get y set de las clases.
+    public int getX() {
         return this.x;
     }
-    public int getY(){
+
+    public int getY() {
         return this.y;
     }
-    public void setX(int valor){
+
+    public void setX(int valor) {
         this.x = valor;
     }
-    public void setY(int valor){
+
+    public void setY(int valor) {
         this.y = valor;
     }
-    ///MOVIMIENTOS DEL CABALLO, CONTANDO EN EL SENTIDO DE LAS MANECILLAS DEL RELOJ
-    public nodo arriba1(){
-        nodo nuevo = new nodo((this.x -1),(this.y+2));///Se crea un nuevo nodo con las coordenadas del movimiento
+
+    /// MOVIMIENTOS DEL CABALLO, CONTANDO EN EL SENTIDO DE LAS MANECILLAS DEL RELOJ
+    public nodo arriba1() {
+        nodo nuevo = new nodo((this.x - 1), (this.y + 2));/// Se crea un nuevo nodo con las coordenadas del movimiento
         nuevo.movimientos = this.movimientos;
         nuevo.movimientos.add(1);
         return nuevo;
     }
-    public nodo arriba2(){
-        nodo nuevo = new nodo((this.x + 1),(this.y+2));///Se crea un nuevo nodo con las coordenadas del movimiento
+
+    public nodo arriba2() {
+        nodo nuevo = new nodo((this.x + 1), (this.y + 2));/// Se crea un nuevo nodo con las coordenadas del movimiento
         nuevo.movimientos = this.movimientos;
         nuevo.movimientos.add(2);
         return nuevo;
-        
+
     }
-    public nodo derecha1(){
-        nodo nuevo = new nodo((this.x + 2),(this.y + 1)); ///Se cre aun nuevo nodo con las coordenadas del movimiento
+
+    public nodo derecha1() {
+        nodo nuevo = new nodo((this.x + 2), (this.y + 1)); /// Se cre aun nuevo nodo con las coordenadas del movimiento
         nuevo.movimientos = this.movimientos;
         nuevo.movimientos.add(3);
         return nuevo;
     }
-    public nodo derecha2(){
-        nodo nuevo = new nodo((this.x + 2),(this.y - 1)); ///Se cre aun nuevo nodo con las coordenadas del movimiento
+
+    public nodo derecha2() {
+        nodo nuevo = new nodo((this.x + 2), (this.y - 1)); /// Se cre aun nuevo nodo con las coordenadas del movimiento
         nuevo.movimientos = this.movimientos;
         nuevo.movimientos.add(4);
         return nuevo;
     }
-    public nodo abajo1(){
-        nodo nuevo = new nodo((this.x + 1),(this.y - 2)); ///Se cre aun nuevo nodo con las coordenadas del movimiento
+
+    public nodo abajo1() {
+        nodo nuevo = new nodo((this.x + 1), (this.y - 2)); /// Se cre aun nuevo nodo con las coordenadas del movimiento
         nuevo.movimientos = this.movimientos;
         nuevo.movimientos.add(5);
         return nuevo;
     }
-    public nodo abajo2(){
-        nodo nuevo = new nodo((this.x - 1),(this.y - 2)); ///Se cre aun nuevo nodo con las coordenadas del movimiento
+
+    public nodo abajo2() {
+        nodo nuevo = new nodo((this.x - 1), (this.y - 2)); /// Se cre aun nuevo nodo con las coordenadas del movimiento
         nuevo.movimientos = this.movimientos;
         nuevo.movimientos.add(6);
         return nuevo;
     }
-    public nodo izquierda1(){
-        nodo nuevo = new nodo((this.x - 2),(this.y - 1)); ///Se cre aun nuevo nodo con las coordenadas del movimiento
+
+    public nodo izquierda1() {
+        nodo nuevo = new nodo((this.x - 2), (this.y - 1)); /// Se cre aun nuevo nodo con las coordenadas del movimiento
         nuevo.movimientos = this.movimientos;
         nuevo.movimientos.add(7);
         return nuevo;
     }
-    public nodo izquierda2(){
-        nodo nuevo = new nodo((this.x - 2),(this.y + 1)); ///Se cre aun nuevo nodo con las coordenadas del movimiento
+
+    public nodo izquierda2() {
+        nodo nuevo = new nodo((this.x - 2), (this.y + 1)); /// Se cre aun nuevo nodo con las coordenadas del movimiento
         nuevo.movimientos = this.movimientos;
         nuevo.movimientos.add(8);
         return nuevo;
     }
-    public boolean esValido(){
-        if((this.x<1 || this.x>8) || (this.y<1 || this.y>8)){
+
+    public boolean esValido() {
+        if ((this.x < 1 || this.x > 8) || (this.y < 1 || this.y > 8)) {
             return false;
         }
         return true;
